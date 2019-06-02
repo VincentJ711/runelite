25b314f2191b8087c845a94875ce76398280c9ea
VincentJ711 <vincentj711@gmail.com>
2019-06-02 06:23:11 -0700

    typo

--- runelite-client/src/main/java/net/runelite/client/plugins/wildylevellines/WildyLevelLinesConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/wildylevellines/WildyLevelLinesConfig.java
@@ -5,7 +5,7 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("wilyLevelLines")
+@ConfigGroup("wildyLevelLines")
 public interface WildyLevelLinesConfig extends Config
 {
 	@ConfigItem(
